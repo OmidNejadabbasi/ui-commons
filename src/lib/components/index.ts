0bbdf86f930d6869bf4d29afff469abac49ee3bb
Omid Nejadabbasi <omidne@systemgroup.net>
2024-04-04 15:21:01 +0330

    add package-watch command and fix exports

---
+++ src/lib/components/index.ts
@@ -0,0 +1,7 @@
+import Button from './Button.svelte';
+import Collapsible from './Collapsible.svelte';
+import Dialog from './Dialog.svelte';
+import Space from './Space.svelte';
+import TextField from './TextField.svelte';
+
+export { Button, Collapsible, Dialog, Space, TextField };
