b777e30da5a35ecbbcb01dc51590183aeb41032e
Omid Nejadabbasi <omidne@systemgroup.net>
2024-06-06 12:35:15 +0330

    add message dialog

---
+++ src/lib/components/dialogs/Message.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Dialog from '../Dialog.svelte';
+  import { onDestroy } from 'svelte';
+  import { DialogResult } from '../../utils/index.js';
+
+  let { message, onClose }: { message; onClose } = $props();
+  let isOpen = $state(true);
+  function closeWithRes(res: DialogResult) {
+    isOpen = false;
+    onClose(res);
+  }
+</script>
+
+<Dialog {isOpen}>
+  <div class="min-w-96">
+    <p class="text-lg">{message}</p>
+    <div class="h-4"></div>
+    <div class="flex gap-2">
+      <button
+        class="bg-emerald-400 rounded px-2 py-1"
+        on:click={() => closeWithRes(DialogResult.OK)}
+      >
+        Ok
+      </button>
+      
+    </div>
+  </div>
+</Dialog>
+<style lang="postcss">
+</style>
--- src/lib/utils/dialog-utils.svelte.ts
+++ src/lib/utils/dialog-utils.svelte.ts
@@ -1,3 +1,4 @@
+import Message from '../components/dialogs/Message.svelte';
 import Confirmation from '../components/dialogs/Confirmation.svelte';
 import { mount, unmount } from 'svelte';
 
@@ -7,6 +8,22 @@ export class DialogUtils {
       let message = $state(mes);
       console.log(typeof Confirmation);
       const dialog = mount(Confirmation, {
+        target: document.body,
+        props: {
+          message,
+          onClose: (result) => {
+            res(result.detail);
+            unmount(dialog);
+          }
+        }
+      });
+    });
+  }
+
+  static async message(mes: string): Promise<DialogResult> {
+    return new Promise<DialogResult>((res) => {
+      let message = $state(mes);
+      const dialog = mount(Message, {
         target: document.body,
         props: {
           message,
